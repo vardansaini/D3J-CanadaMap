4ce3182b7a97da1c46fa8430807da698ec4a16ce
vardansaini <vardan1@ualberta.ca>
2023-12-18 19:29:24 -0700

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ function App() {
   const colorGenerator = () => {
     return colorScale[Math.floor(Math.random() * 4)];
   };
+  const [result, setResult] = React.useState([]);
+  const yasrRef = React.useRef();
 
   useEffect(() => {
     let width = parseInt(d3.select(".viz").style("width"));
@@ -65,24 +67,6 @@ function App() {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
 
-    // Creating counties layer
-    // g.append("g")
-    //   .attr("id", "counties")
-    //   .selectAll("path")
-    //   .data(countyData.features)
-    //   .enter()
-    //   .append("path")
-    //   .attr("d", pathGenerator)
-    //   .attr("key", (feature) => {
-    //     return feature.properties.STATE + feature.properties.COUNTY;
-    //   })
-    //   .attr("class", "county-boundary")
-    //   .attr("fill", (feature) => {
-    //     // I could directly call colorGenerator instead of calling it in a arrow function, I've added it in that way so that you'd know we can send values from geo json into every step of the map creation.
-    //     return colorGenerator();
-    //   })
-    //   .on("click", resetZoom);
-
     // Creating state layer on top of counties layer.
     g.append("g")
       .attr("id", "states")
@@ -101,61 +85,63 @@ function App() {
       .attr("fill", colorGenerator)
       .on("click", (event, feature) => handleZoom(event, feature));
 
-    function handleZoom(event, feature) {
-      // Set the state backgroud to 'none' so that the counties can be displayed.
-      // active.classed("active", false);
-      // active = d3.select(this).classed("active", true);
-
-      console.log(feature);
-
-      toast.info(`Selected state is ${feature.properties.name}`, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      // Call to zoom in.
-      // zoomIn(featurevar);
-    }
-
-    // function zoomIn(currentState) {
-    //   // Get bounding box values for the selected county.
-    //   let bounds = pathGenerator.bounds(currentState);
-
-    //   // Zoom In calculations
-    //   let dx = bounds[1][0] - bounds[0][0];
-    //   let dy = bounds[1][1] - bounds[0][1];
-
-    //   let x = (bounds[0][0] + bounds[1][0]) / 2;
-    //   let y = (bounds[0][1] + bounds[1][1]) / 2;
-
-    //   let scale = 0.9 / Math.max(dx / width, dy / height);
-    //   let translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-    //   // Updaing the css using D3
-    //   g.transition()
-    //     .duration(750)
-    //     .style("stroke-width", 1.5 / scale + "px")
-    //     .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-    // }
-
-    // function resetZoom() {
-    //   // Remove the active class so that state color will be restored and conuties will be hidden again.
-    //   active.classed("active", false);
-    //   active = d3.select(null);
-
-    //   // Resetting the css using D3
-    //   g.transition()
-    //     .delay(100)
-    //     .duration(750)
-    //     .style("stroke-width", "1.5px")
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    // }
+    yasrRef.current = svg;
   }, []);
+
+  function handleZoom(event, feature) {
+    // Set the state backgroud to 'none' so that the counties can be displayed.
+    // active.classed("active", false);
+    // active = d3.select(this).classed("active", true);
+
+    console.log(feature);
+
+    toast.info(`Selected state is ${feature.properties.name}`, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    // Call to zoom in.
+    // zoomIn(featurevar);
+  }
+
+  // function zoomIn(currentState) {
+  //   // Get bounding box values for the selected county.
+  //   let bounds = pathGenerator.bounds(currentState);
+
+  //   // Zoom In calculations
+  //   let dx = bounds[1][0] - bounds[0][0];
+  //   let dy = bounds[1][1] - bounds[0][1];
+
+  //   let x = (bounds[0][0] + bounds[1][0]) / 2;
+  //   let y = (bounds[0][1] + bounds[1][1]) / 2;
+
+  //   let scale = 0.9 / Math.max(dx / width, dy / height);
+  //   let translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+  //   // Updaing the css using D3
+  //   g.transition()
+  //     .duration(750)
+  //     .style("stroke-width", 1.5 / scale + "px")
+  //     .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+  // }
+
+  // function resetZoom() {
+  //   // Remove the active class so that state color will be restored and conuties will be hidden again.
+  //   active.classed("active", false);
+  //   active = d3.select(null);
+
+  //   // Resetting the css using D3
+  //   g.transition()
+  //     .delay(100)
+  //     .duration(750)
+  //     .style("stroke-width", "1.5px")
+  //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // }
 
   return (
     <div>
